5c686983de4c33ac9f911a4b7c5a6e0abe3eaacd
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-04-29 11:17:06 +0300

    create async handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// src/index.ts
 import express from 'express'
 import Config from './config/environment'
+import ErrorHandler from './middlewares/error-handler'
 
 const app = express()
 app.use(express.json())
@@ -9,6 +9,9 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!')
 })
+
+// Error handler
+app.use(ErrorHandler)
 
 app.listen(Config.server.port, () => {
   console.log(`Server is running on http://localhost:${Config.server.port}`)
