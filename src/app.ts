7671e88907a6c4493ad6b1f645aa24cd9b4b7aff
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-04-29 12:58:59 +0300

    setup ejs view engine

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path'
+
 import ErrorHandler from './middlewares/error-handler'
 import { NotFoundError } from './utils/error/NotFoundError'
 import { clientErrorHandler } from './middlewares/client-error-handler'
@@ -7,9 +9,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+// Set views directory
+app.set('views', path.join(__dirname, 'views'))
+
+// Hello world
 app.get('/', (req, res) => {
-  throw new NotFoundError('This is an error')
-  res.send('Hello, TypeScript with Express!')
+  res.render('pages/index', {})
 })
 
 // client error handler
