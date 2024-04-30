2b5f1ea94b0edd49a6b7421b83ad38bd7417ec5f
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-04-30 13:44:32 +0300

    validator middleware

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,34 @@
+import { Schema, ValidationError } from 'yup'
+import asyncHandler from '../utils/async-handler'
+import { NextFunction, Request, Response } from 'express'
+import { BadRequestError } from '../utils/error/BadRequestError'
+
+export function validate(schema: Schema<any>) {
+  return asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        console.log('validation test')
+        console.log({ ...req.body })
+
+        const data = await schema.validate({
+          body: req.body,
+          query: req.query,
+          params: req.params,
+        })
+
+        console.log({ ...data })
+
+        // replace with the validated data
+        req.body = { ...data.body }
+        req.query = { ...data.query }
+        req.params = { ...data.params }
+        next()
+      } catch (error) {
+        if (error instanceof ValidationError) {
+          throw new BadRequestError(error.errors[0])
+        }
+        throw new BadRequestError()
+      }
+    }
+  )
+}
