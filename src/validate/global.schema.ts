3297ce022e6fd476791729d9e6bb2d4cc01cf825
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-01 15:08:59 +0300

    pagination validation schema

--- src/validate/global.schema.ts
+++ src/validate/global.schema.ts
@@ -9,3 +9,30 @@ export const idParamSchema = Yup.object({
       .required(),
   }),
 })
+
+export const paginationSchema = Yup.object({
+  query: Yup.object({
+    page: Yup.number()
+      .label('page')
+      .integer()
+      .positive()
+      .default(1)
+      .transform((value) => (isNaN(value) ? undefined : value))
+      .transform((value) => {
+        if (value < 1) return 1
+        else return value
+      })
+      .required(),
+    page_size: Yup.number()
+      .label('page size')
+      .integer()
+      .positive()
+      .default(20)
+      .transform((value) => (isNaN(value) ? undefined : value))
+      .transform((value, originalValue) => {
+        if (originalValue > 100) return 100
+        else if (originalValue < 1) return 20
+        else return value
+      }),
+  }),
+})
