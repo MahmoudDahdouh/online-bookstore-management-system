1ec38a5a4814fa629c3d8ebec431279b350e77de
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-02 17:15:18 +0300

    update book schema

--- src/routes/api/book.router.ts
+++ src/routes/api/book.router.ts
@@ -9,14 +9,18 @@ import {
 } from '../../controllers/book.controller'
 import asyncify from 'express-asyncify'
 import { validate } from '../../middlewares/validate'
-import { createBookSchema } from '../../validate/book.schema'
+import { createBookSchema, updateBookSchema } from '../../validate/book.schema'
 
 const router = asyncify(Router())
 
 router.post('/', validate(createBookSchema), createBook)
 router.get('/', validate(paginationSchema), getAllBooks)
 router.get('/:id', validate(idParamSchema), getBookById)
-router.patch('/:id', updateBook)
+router.patch(
+  '/:id',
+  [validate(idParamSchema), validate(updateBookSchema)],
+  updateBook
+)
 router.delete('/', deleteBook)
 
 export default router
--- src/validate/book.schema.ts
+++ src/validate/book.schema.ts
@@ -21,3 +21,58 @@ export const createBookSchema = Yup.object({
       .required(),
   }),
 })
+
+export const updateBookSchema = Yup.object({
+  body: Yup.object({
+    title: Yup.string()
+      .label('title')
+      .trim()
+      .min(3)
+      .max(255)
+      .transform((value) => (value == '' ? undefined : value)),
+    description: Yup.string()
+      .label('description')
+      .trim()
+      .min(3)
+      .max(5000)
+      .transform((value) => (value == '' ? undefined : value)),
+    author: Yup.string()
+      .label('author')
+      .trim()
+      .min(3)
+      .max(255)
+      .transform((value) => (value == '' ? undefined : value)),
+    genre: Yup.string()
+      .label('genre')
+      .trim()
+      .min(3)
+      .max(64)
+      .transform((value) => (value == '' ? undefined : value)),
+    language: Yup.string()
+      .label('language')
+      .trim()
+      .min(3)
+      .max(64)
+      .transform((value) => (value == '' ? undefined : value)),
+    isbn: Yup.string()
+      .label('isbn')
+      .length(13)
+      .nullable()
+      .transform((value) => (value == '' ? undefined : value)),
+    price: Yup.number()
+      .label('price')
+      .positive()
+      .nullable()
+      .transform((value) => (isNaN(value) ? undefined : value)),
+    page_count: Yup.number()
+      .label('page count')
+      .positive()
+      .nullable()
+      .transform((value) => (isNaN(value) ? undefined : value)),
+    published_date: Yup.date()
+      .label('published date')
+      .nullable()
+      .transform((value) => (isNaN(value) ? undefined : value))
+      .max(new Date()),
+  }),
+})
