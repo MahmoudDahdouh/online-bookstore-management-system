f3ac8b34ac8c6c6d0395058c86692c4804e1faa0
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-04 12:57:41 +0300

    verify token utils function

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,15 @@
 import jwt from 'jsonwebtoken'
 import Config from '../config/environment'
+import { UnauthorizedError } from './error/UnauthorizedError'
 
 export function generateToken(payload: object, options: object) {
   return jwt.sign(payload, Config.jwt.access_secret_key, options)
+}
+
+export function verifyToken(accessToken: string) {
+  try {
+    return jwt.verify(accessToken, Config.jwt.access_secret_key)
+  } catch (error) {
+    throw new UnauthorizedError('Expired token')
+  }
 }
