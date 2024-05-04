8e0a87e99fbe617cf2d289a7181ddecf635d981d
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-04 13:59:01 +0300

    required role middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { UnauthorizedError } from '../utils/error/UnauthorizedError'
 import { verifyToken } from '../utils/jwt'
+import { ForbiddenError } from '../utils/error/ForbiddenError'
 
 export const requireAuth = (
   req: Request,
@@ -17,4 +18,14 @@ export const requireAuth = (
   const payload = verifyToken(accessToken)
   req.body.user = payload
   next()
+}
+
+export const requiredRole = (roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const role = req.body.user.role
+    if (role in roles) {
+      next()
+    }
+    throw new ForbiddenError('You are not allowed')
+  }
 }
