24fffed421779de91406c36ec1d0fd108095c5d4
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-07 13:03:44 +0300

    add page title

--- src/routes/views/index.ts
+++ src/routes/views/index.ts
@@ -4,17 +4,21 @@ const router = Router()
 
 // index page
 router.get('/', (req, res) => {
-  res.render('pages/index', {})
+  res.render('pages/index', { title: 'index' })
 })
 
 // login page
 router.get('/login', (req, res) => {
-  res.render('pages/login')
+  res.render('pages/login', { title: 'login' })
 })
 
 // register page
 router.get('/register', (req, res) => {
-  res.render('pages/register')
+  res.render('pages/register', { title: 'register' })
 })
 
+// home page
+router.get('/home', (req, res) => {
+  res.render('pages/home', { title: 'home' })
+})
 export default router
