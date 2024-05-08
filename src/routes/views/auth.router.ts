6c44e5907a2bc00d8f6676baaee3e6e9fe2f4813
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-08 11:26:06 +0300

    store a session when login or register

--- src/routes/views/auth.router.ts
+++ src/routes/views/auth.router.ts
@@ -1,55 +1,79 @@
 import { Router } from 'express'
-import { getUserByEmail } from '../../services/auth.service'
-import bcrypt from 'bcrypt'
-import { generateToken } from '../../utils/jwt'
-import { login } from '../../controllers/auth.controller'
+import { isValidToken } from '../../utils/jwt'
 
 const router = Router()
 
-// login page
-router
-  .get('/login', async (req, res) => {
-    res.render('pages/login', { title: 'login', error: req.query.error })
+/**
+ * Login page
+ */
+router.get('/login', (req, res) => {
+  // check if there is a user in the session
+  if (req.session.user && isValidToken(req.session.user.token)) {
+    return res.redirect('/home')
+  }
+  return res.render('pages/login', {
+    title: 'login',
+    error: req.query.error,
   })
-  .post('/login', async (req, res) => {
-    const { email, password } = req.body
+})
 
-    const user = await fetch('http://localhost:3000/api/v1.0/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    })
+// login
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
 
-    const data = await user.json()
-    if (data.success) {
-      return res.redirect('/home')
-    }
-    return res.redirect(`/login?error=${data.message}`)
+  const user = await fetch('http://localhost:3000/api/v1.0/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
   })
 
-// register page
-router
-  .get('/register', (req, res) => {
-    res.render('pages/register', { title: 'register', error: req.query.error })
-  })
-  .post('/register', async (req, res) => {
-    const { email, password, username } = req.body
+  const data = await user.json()
+  if (data.success) {
+    // create a session
+    console.log({ data })
 
-    const user = await fetch('http://localhost:3000/api/v1.0/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password, username }),
-    })
+    req.session.user = { ...data.user, token: data.access_token }
+    return res.redirect('/home')
+  }
+  return res.redirect(`/login?error=${data.message}`)
+})
 
-    const data = await user.json()
-    if (data.success) {
-      return res.redirect('/home')
-    }
-    return res.redirect(`/register?error=${data.message}`)
+/**
+ * Register page
+ */
+router.get('/register', (req, res) => {
+  // check if there is a user in the session
+  if (req.session.user && isValidToken(req.session.user.token)) {
+    return res.redirect('/home')
+  }
+  return res.render('pages/register', {
+    title: 'register',
+    error: req.query.error,
   })
+})
+
+// register
+router.post('/register', async (req, res) => {
+  const { email, password, username } = req.body
+
+  const user = await fetch('http://localhost:3000/api/v1.0/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password, username }),
+  })
+
+  const data = await user.json()
+  if (data.success) {
+    console.log({ data })
+
+    req.session.user = { ...data.user, token: data.token }
+    return res.redirect('/home')
+  }
+  return res.redirect(`/register?error=${data.message}`)
+})
 
 export default router
