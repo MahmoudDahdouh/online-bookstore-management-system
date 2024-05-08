e07563d504cc57fb231c32d99a44ce58344ee094
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-08 11:52:36 +0300

    add user to profile router

--- src/routes/views/profile.router.ts
+++ src/routes/views/profile.router.ts
@@ -4,17 +4,20 @@ const router = Router()
 
 // profile
 router.get('/', (req, res) => {
-  res.render('pages/profile', { title: 'profile' })
+  res.render('pages/profile', { title: 'profile', user: req.session.user })
 })
 
 // edit profile
 router.get('/edit', (req, res) => {
-  res.render('pages/profile_edit', { title: 'edit profile' })
+  res.render('pages/profile_edit', {
+    title: 'edit profile',
+    user: req.session.user,
+  })
 })
 
 // orders
 router.get('/orders', (req, res) => {
-  res.render('pages/orders', { title: 'orders' })
+  res.render('pages/orders', { title: 'orders', user: req.session.user })
 })
 
 export default router
