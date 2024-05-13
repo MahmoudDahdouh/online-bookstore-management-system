c802b369553bad15339a9da46bcb931a37c61a6f
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-05-13 14:13:54 +0300

    add order router

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import authRouter from './auth.router'
 import bookRouter from './book.router'
+import orderRouter from './order.router'
 import { requireAuth } from '../../middlewares/auth.middleware'
 
 const router = Router()
@@ -10,5 +11,8 @@ router.use('/auth', authRouter)
 
 // book routes
 router.use('/book', [requireAuth], bookRouter)
+
+// order routes
+router.use('/order', [requireAuth], orderRouter)
 
 export default router
